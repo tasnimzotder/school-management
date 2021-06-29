9e6d31ac7f3a46a39b334d7bb83f331e3e8459cb
Tasnim Zotder <hello@tasnim.dev>
2021-06-29 14:54:53 +0530

    :art: add pages

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,106 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+import React from 'react';
+import Router from 'next/router';
+
+import { withStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import NativeSelect from '@material-ui/core/NativeSelect';
+import InputBase from '@material-ui/core/InputBase';
+
+const BootstrapInput = withStyles((theme) => ({
+  root: {
+    'label + &': {
+      marginTop: theme.spacing(3),
+    },
+  },
+  input: {
+    borderRadius: 4,
+    position: 'relative',
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid #ced4da',
+    fontSize: 16,
+    padding: '10px 26px 10px 12px',
+    transition: theme.transitions.create(['border-color', 'box-shadow']),
+    '&:focus': {
+      borderRadius: 4,
+      borderColor: '#80bdff',
+      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
+    },
+  },
+}))(InputBase);
 
 export default function Home() {
+  const [loginType, setLoginType] = React.useState('');
+  const handleChange = (event) => {
+    setLoginType(event.target.value);
+  };
+
+  const handleLogin = () => {
+    if (loginType == 0) {
+      Router.push('/student');
+    }
+  };
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+        />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div className={styles.card}>
+          <div className={styles.card_header}>
+            <span>Login Here</span>
+          </div>
+          <hr></hr>
+          <div className={styles.card_body}>
+            <FormControl className={styles.form_ctrl}>
+              <InputLabel htmlFor="demo-customized-textbox">
+                Registration ID
+              </InputLabel>
+              <BootstrapInput id="demo-customized-textbox" />
+            </FormControl>
+            <FormControl className={styles.form_ctrl}>
+              <InputLabel id="demo-customized-select-label">
+                Login type
+              </InputLabel>
+              <Select
+                labelId="demo-customized-select-label"
+                id="demo-customized-select"
+                value={loginType}
+                onChange={handleChange}
+                input={<BootstrapInput />}
+              >
+                <MenuItem value={0}>Student</MenuItem>
+                <MenuItem value={1}>Teacher</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className={styles.card_actions}>
+            <button onClick={handleLogin}>Login</button>
+          </div>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        <div>
+          <span>School Management System</span>
+        </div>
       </footer>
     </div>
-  )
+  );
 }
---
+++ pages/student/index.js
@@ -0,0 +1,61 @@
+import styles from '../../styles/Student.module.css';
+
+export default function StudentHome() {
+  return (
+    <div>
+      <div className={styles.header}>Faculty Details</div>
+      <div className={styles.faculty_table}>
+        <table>
+          <thead>
+            <td>Sl. No</td>
+            <td>Name</td>
+            <td>ID</td>
+            <td>Cabin No.</td>
+            <td>Free Time Slot</td>
+            <td>Specialization</td>
+            <td>Time Table</td>
+            <td>Book</td>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Gurpreet Singh</td>
+              <td>789632</td>
+              <td>52-305C</td>
+              <td>09:00-10:30</td>
+              <td>Networking</td>
+              <td>09:00-17:00</td>
+              <td>
+                <button className={styles.book_btn}>Book</button>
+              </td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Manpreet Singh</td>
+              <td>456985</td>
+              <td>52-305D</td>
+              <td>09:30-11:30</td>
+              <td>Web Development</td>
+              <td>09:00-17:00</td>
+              <td>
+                <button className={styles.book_btn}>Book</button>
+              </td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Gurpreet Singh</td>
+              <td>789632</td>
+              <td>52-305C</td>
+              <td>09:00-10:30</td>
+              <td>Networking</td>
+              <td>09:00-17:00</td>
+              <td>
+                <button className={styles.book_btn}>Book</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
